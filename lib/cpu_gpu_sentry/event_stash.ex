bb21ac7515a1f3ba6e255ed9bf28c53c16241f4d
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-04-01 13:04:48 +0700

    add event stash.

---
+++ lib/cpu_gpu_sentry/event_stash.ex
@@ -0,0 +1,36 @@
+defmodule CpuGpuSentry.EventStash do
+  use GenServer
+  require Logger
+
+  def start_link(_args), do: CpuGpuSentry.EventStash.start_link()
+  def start_link() do
+    {:ok, pid} = GenServer.start_link(__MODULE__, nil)
+    pid_string = pid
+    |> :erlang.pid_to_list()
+    |> to_string()
+    Logger.info("[CpuGpuSentry.EventStash] PID:#{pid_string} Started ")
+    {:ok, pid}
+  end
+
+  @impl true
+  def init(_args) do
+    state = []
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_cast({:append, event}, state) do
+    new_state = state ++ [event]
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_call(:pop, _from, state) do
+    if Kernel.length(state) == 0 do
+      {:reply, nil, []}
+    else
+      [event | other_event_list] = state
+      {:reply, event, other_event_list}
+    end
+  end
+end
